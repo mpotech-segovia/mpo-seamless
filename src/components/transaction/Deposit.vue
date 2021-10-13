9d105224d8844e7ca7518313e020bf6229303706
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2021-10-13 10:44:04 +0800

    deposit > deposit history table

--- src/components/transaction/Deposit.vue
+++ src/components/transaction/Deposit.vue
@@ -168,7 +168,11 @@
                     <v-icon small class="mr-2">fas fa-trash</v-icon> Delete Selected
                 </v-btn>
             </div>
-            <v-data-table :headers="headers" :items="desserts" :items-per-page="5" mobile-breakpoint="0"></v-data-table>
+            <v-data-table :headers="headers" :items="depositData" :items-per-page="5" mobile-breakpoint="0"
+            :footer-props="{
+                prevIcon: 'fas fa-angle-left',
+                nextIcon: 'fas fa-angle-right'
+            }"></v-data-table>
         </div>
     </div>
 </template>
@@ -251,101 +255,64 @@
                         clickable: true
                     },
                 },
-                headers: [
-                {
-                    text: 'Dessert (100g serving)',
+                headers: [{
+                    text: 'Nomor',
                     align: 'start',
                     sortable: false,
-                    value: 'name',
+                    divider: true,
+                    value: 'number',
+                }, 
+                { 
+                    text: 'Tanggal', 
+                    divider: true,
+                    value: 'date',
+                }, 
+                { 
+                    text: 'Jumlah', 
+                    divider: true,
+                    value: 'amount',
                 },
-                { text: 'Calories', value: 'calories' },
-                { text: 'Fat (g)', value: 'fat' },
-                { text: 'Carbs (g)', value: 'carbs' },
-                { text: 'Protein (g)', value: 'protein' },
-                { text: 'Iron (%)', value: 'iron' },
-                ],
-                desserts: [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: '1%',
-                },
-                {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    iron: '1%',
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    iron: '7%',
+                { 
+                    text: 'Status', 
+                    divider: true,
+                    value: 'status',
+                },],
+                depositData: [{
+                    number: '1',
+                    date: 'October 13, 2021',
+                    amount: '999.99',
+                    status: 'Approved',
                 },
                 {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    iron: '8%',
+                    number: '2',
+                    date: 'October 14, 2021',
+                    amount: '999.99',
+                    status: 'Pending',
                 },
                 {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    iron: '16%',
+                    number: '3',
+                    date: 'October 15, 2021',
+                    amount: '999.99',
+                    status: 'Rejected',
                 },
                 {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    iron: '0%',
+                    number: '1',
+                    date: 'October 13, 2021',
+                    amount: '999.99',
+                    status: 'Approved',
                 },
                 {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    iron: '2%',
+                    number: '2',
+                    date: 'October 14, 2021',
+                    amount: '999.99',
+                    status: 'Pending',
                 },
                 {
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                    iron: '45%',
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                    iron: '22%',
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                    iron: '6%',
-                },
-                ],
+                    number: '3',
+                    date: 'October 15, 2021',
+                    amount: '999.99',
+                    status: 'Rejected',
+                },],
             }
         }
     }
